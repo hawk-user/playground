e602a69c9fb599542326341bab34e9a531b7b7b0
Salem <salem.djekoun@gmail.com>
2025-11-25 15:34:20 +0100

    chore: implementation of the BaseRuntime

---
+++ clients/webapp/server/infra/http/models/base.runtime.ts
@@ -0,0 +1,30 @@
+import { 
+    type CommonRuntime
+} from '@libs/infra/contracts';
+
+import nodeUrl from 'node:url';
+import nodePath from 'node:path';
+import nodeFs from 'node:fs';
+
+export class BaseRutime implements CommonRuntime {
+
+    public fileURLToPath (url: string): string {
+        return nodeUrl.fileURLToPath(url);
+    }
+
+    public pathToDirname (path: string): string {
+        return nodePath.dirname(path);
+    }
+
+    public readFile (path: string) {
+        return nodeFs.readFileSync(path, { encoding: 'utf-8' });
+    }
+
+    public resolvePath (path: string): string {
+        const dirname = this.pathToDirname(
+            this.fileURLToPath(import.meta.url));
+        return nodePath.resolve(dirname, path);
+    };
+
+}
+
