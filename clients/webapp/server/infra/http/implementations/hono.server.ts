c93c385998c1f5cc4c14092d416ec0fd0ba2ddfc
Salem <salem.djekoun@gmail.com>
2025-11-26 12:47:55 +0100

    chore: make independent from Node

--- clients/webapp/server/infra/http/implementations/hono.server.ts
+++ clients/webapp/server/infra/http/implementations/hono.server.ts
@@ -4,26 +4,51 @@ import {
     type Handler
 } from '@libs/infra/contracts';
 
-import { NodeJSRutime } from './nodejs.runtime';
+import { type CommonLogger } from '@libs/core';
 import { serve } from '@hono/node-server';
 
 import { 
-    Hono,
-    type Context,
-    type Next 
+    Hono, 
+    type Context as HonoContext,
+    type Next as HonoNext 
 } from 'hono';
 
-export class HonoServer implements CommonServer<Context, Next> {
+export class HonoServer implements CommonServer<
+    HonoContext,
+    HonoNext
+> {
 
-    runtime: CommonRuntime = new NodeJSRutime();
-    app: Hono = new Hono();
+    
+    private app: Hono;
+    private logger: CommonLogger;
+    protected runtime: CommonRuntime;
 
-    public start = (): void => { serve(this.app) };
+    private constructor (
+        runtime: CommonRuntime,
+        logger: CommonLogger
+    ) {
+        this.app = new Hono();
+        this.logger = logger;
+        this.runtime = runtime;
+    }
+
+    public startOn (port: number) {
+        const options = { port, fetch: this.app.fetch };
+        const message = `The server listens on port ${port}.`
+        serve(options, () => this.logger.informational(message));
+    };
 
     public usePreHandler<G> (
-        handler: Handler<Context, Next, G>
+        handler: Handler<HonoContext, HonoNext, G>
     ): void {
         this.app.use('*', handler);
+    }
+
+    public static use(
+        runtime: CommonRuntime,
+        logger: CommonLogger
+    ) {
+        return new HonoServer(runtime, logger);
     }
 
 }
