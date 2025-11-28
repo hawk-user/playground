17011177ce7d74a6ece9d2dac3d595c137608450
Salem <salem.djekoun@gmail.com>
2025-11-28 10:24:38 +0100

    chore: enhance CoreLogger

--- libs/core/source/core.logger.ts
+++ libs/core/source/core.logger.ts
@@ -1,9 +1,23 @@
-import { type ReasonsToCommunicate } from './communication';
+import { type ReasonForBeing } from './communication';
 
-export interface CoreLogger {
-    fatal: ReasonsToCommunicate<[message: string], void>;
-    error: ReasonsToCommunicate<[message: string], void>;
-    warning: ReasonsToCommunicate<[message: string], void>;
-    informational: ReasonsToCommunicate<[message: string], void>;
-    debug: ReasonsToCommunicate<[message: string], void>;
+export type CoreLoggerFatal = ReasonForBeing<[message: string], void>;
+export type CoreLoggerError = ReasonForBeing<[message: string], void>;
+export type CoreLoggerWarning = ReasonForBeing<[message: string], void>;
+export type CoreLoggerInformational = ReasonForBeing<[message: string], void>;
+export type CoreLoggerDebug = ReasonForBeing<[message: string], void>;
+
+export abstract class CoreLogger {
+
+    protected readonly isDev?: boolean;
+
+    abstract fatal: CoreLoggerFatal;
+    abstract error: CoreLoggerError;
+    abstract warning: CoreLoggerWarning;
+    abstract informational: CoreLoggerInformational;
+    abstract debug: CoreLoggerDebug;
+
+    protected constructor (isDev?: boolean) {
+        this.isDev = isDev;
+    }
+    
 }
