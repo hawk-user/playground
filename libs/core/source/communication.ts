89858be6b136b7723626a565bf2f740f883d431c
Salem <salem.djekoun@gmail.com>
2025-11-28 10:18:12 +0100

    chore: use a more evocative name

--- libs/core/source/communication.ts
+++ libs/core/source/communication.ts
@@ -1,7 +1,7 @@
-export type ReasonsToCommunicate<A extends unknown[] = unknown[], R = unknown> = (...args: A) => R;
+export type ReasonForBeing<A extends unknown[] = unknown[], R = unknown> = (...args: A) => R;
 
 export type IsValidPort<T> = T extends (...args: infer A) => infer R
-    ? ReasonsToCommunicate<A, R>
+    ? ReasonForBeing<A, R>
     : never;
 
 export type Port<T extends { [K in keyof T]: IsValidPort<T[K]> }> = Readonly<T>;
